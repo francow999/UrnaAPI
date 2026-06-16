4452497da4875459381128378a6cc190d7d307db
Gabriel Franco <gabrielfrancoaom@gmail.com>
2026-06-16 13:17:16 -0300

    Adição de comentários para clarificar cada rota.

--- src/routes/alunosRoutes.js
+++ src/routes/alunosRoutes.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
+/*
+Usamos o router para listar as funções dos controllers e depois
+enviamos esse router para o app, com o objetivo de centralizar tudo.
+*/
+
+// Puxamos as funções de MiddleWare e Token
 const { VerificarToken, VerificarAdmin, VerificarAuditor } = require('../middlewares/auth');
+// Junto com as funções do controller específico (Nesse caso, os alunos).
 const { listarAlunos, cadastrarAlunos } = require('../controllers/alunosController');
 
+//Então, passamos os middlewares com as funções no determinado papel (get, post, etc).
 router.get('/', VerificarToken, VerificarAuditor, listarAlunos);
 router.post('/', VerificarToken, VerificarAdmin, cadastrarAlunos);
 
