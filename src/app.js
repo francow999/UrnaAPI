9eecd201e5ca6fff4eeaa79acd74ceeeb4666d49
Gabriel Franco <gabrielfrancoaom@gmail.com>
2026-06-16 13:18:32 -0300

    Adição de comentários junto com a rota dos resultados.

--- src/app.js
+++ src/app.js
@@ -4,10 +4,22 @@ app.use(express.json());
 
 // Intermediário entre o controller e o próprio server.
 
+//Alunos
 app.use('/alunos', require('./routes/alunosRoutes'));
+
+//Professores
 app.use('/professores', require('./routes/professoresRoutes'));
+
+//Votos
 app.use('/votos', require('./routes/votosRoutes'));
+
+//Autenticação
 app.use('/auth', require('./routes/authRoutes'));
+
+//Usuários
 app.use('/usuarios', require('./routes/usuariosRoutes'));
+
+//Resultados
+app.use('/resultados', require('./routes/resultadosRoutes'));
 
 module.exports = app;
