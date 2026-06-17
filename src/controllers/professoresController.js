60772b2bb7e571e1b3b0ce50c5c3a64a3c081bfc
Gabriel Franco <gabrielfrancoaom@gmail.com>
2026-06-17 14:47:10 -0300

    Correção do comando DELETE.

--- src/controllers/professoresController.js
+++ src/controllers/professoresController.js
@@ -32,13 +32,28 @@ async function cadastrarProfessores(req, res) {
 async function deletarProfessores(req, res) {
     try{
         const {id} = req.params;
-        await db.query("DELETE FROM tb_votos WHERE id = $1", [id]);
-        const resultado = await db.query("DELETE FROM tb_professores WHERE id = $1 RETURNING *", [id]);
-        if (resultado.rowCount == 0)
+
+        //Checamos se o professor existe
+        const professor = await db.query("SELECT * FROM tb_professores WHERE id = $1", [id]);
+        if (professor.rowCount === 0)
         {
-            return res.status(404).json({ mensagem: 'Não há professores a serem deletados.'});
+            return res.status(404).json({erro: 'Professor não encontrado.'});
         }
-        res.json({ mensagem: `Professor ${resultado.rows[0].nome} e seus votos foram removidos.`});
+
+        //Se existe, verificamos se existem votos relacionados.
+        const votos = await db.query("SELECT COUNT(*) FROM tb_votos WHERE professor_id = $1", [id]);
+        const total_votos = parseInt(votos.rows[0].count);
+        
+        //Se possui, retiramos os votos
+        if (total_votos > 0)
+        {
+            await db.query("DELETE FROM tb_votos WHERE id = $1", [id]);
+        }
+
+        //E então, deletamos o professor.
+        const resultado = await db.query("DELETE FROM tb_professores WHERE id = $1 RETURNING *", [id]);
+        res.json({ mensagem: `Professor ${resultado.rows[0].nome} e seus votos foram removidos.
+            Total de votos removidos: ${total_votos}`});
     } catch(erro)
     {
         console.error(erro);
