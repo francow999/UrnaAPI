ff94aae34b174eb47cd92622ff250d56c96875b8
Gabriel Franco <gabrielfrancoaom@gmail.com>
2026-08-05 16:46:33 -0300

    Implementação da autenticação dos alunos

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,70 +2,106 @@
 const db = require('../config/db');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const {enviarEmail} = require('../services/emailService');
+const crypto = require('crypto');
 
 // Autenticaçã de Alunos ainda em construção.
 
-/*
-async function authGoogle(req, res) {
-    const { idToken } = req.body;
+async function solicitarOTP(req, res) {
     try
     {
-        const ticket = await client.verifyIdToken({
-            idToken: idToken,
-            audience: process.env.GOOGLE_CLIENT_ID,
+        const {ra} = req.body;
+        
+        //OTP + OTP_Data
+        const codigo = crypto.randomInt(100000, 999999);
+        const dataCriacao = (new Date()).toISOString();
+        const verifica = await db.query('SELECT * FROM tb_alunos WHERE ra = $1', [ra]);  
+        //Verificamos se existe o código OTP para evitar emails desnecessários...
+        if (verifica.rows.length === 0)
+        {
+            return res.status(404).json({mensagem: 'Aluno não encontrado'});
+        }
+        const Aluno = verifica.rows[0];
+        if (Aluno.otp_data !== null)
+        {
+            const otpdata = new Date(Aluno.otp_data);
+            const agora = new Date();
+            const diferenca = Math.abs(otpdata.getTime() - agora.getTime());
+
+            if (Aluno.otp !== null && diferenca < 600000)
+            {
+                return res.status(400).json({erro: `O OTP já existe! Verifique a caixa de entrada
+                    de emails ou espere 10 minutos para solicitar outro código.`});
+            }
+
+        }
+        if(Aluno.votou)
+        {
+            return res.status(403).json({erro: `Aluno já votou nesta eleição.`});
+        }
+        //Em seguida, mandamos um código para o email do RA fornecido e também marcamos os dados no banco.
+        const banco = await db.query(`UPDATE tb_alunos SET otp = $1, otp_data = $2 WHERE ra = $3`, [codigo, dataCriacao, ra]);
+        await enviarEmail(ra, codigo);
+        res.status(200).json({
+            mensagem: 'OTP solicitado com sucesso!'
         });
+    } catch(erro)
+    {
+        console.log(erro);
+        res.status(500).json({erro: 'Não foi possível solicitar o OTP.'});
+    }
+}
 
-        const payload = ticket.getPayload();
-        const emailDoGoogle = payload.email;
-        const nomeDoGoogle = payload.name;
-
-        if (!emailDoGoogle.endsWith('@al.educacao.sp.gov.br'))
+async function verificarOTP(req, res)
+{
+    //Necessário verificar:
+    //Comparação entre a data do otp e a data de verificação
+    //Verificar se o OTP está de acordo.
+    try {
+        const {ra, otp} = req.body;
+        const verifica_otp = await db.query('SELECT * FROM tb_alunos WHERE otp = $1 AND ra = $2', [otp, ra]);
+        if (verifica_otp.rows.length === 0)
         {
-            return res.status(403).json({ mensagem: 'Acesso negado. Use o email institucional.' });
+            return res.status(400).json({mensagem: 'Não foi possível encontrar o OTP.'});
         }
-        //0000(RA)sp@al.educacao.sp.gov.br
-        const resultadoRegex = emailDoGoogle.match(/^0000(\d+)sp@/);
-        const raExtraido = resultadoRegex ? resultadoRegex[1] : null;
-
-        if (!raExtraido)
+        const Aluno = verifica_otp.rows[0];
+        const otpdata = new Date(Aluno.otp_data);
+        const agora = new Date();
+        const diferenca = Math.abs(otpdata.getTime() - agora.getTime());
+        if(diferenca >= 600000)
         {
-            return res.status(400).json({ erro: 'Formato de email institucional inválido.'});
+            return res.status(400).json({mensagem: 'O OTP passou de dez minutos! Crie outra requisição de email.'});
         }
-        const buscaAluno = await db.query('SELECT * FROM tb_alunos WHERE tb_alunos.ra =  $1', [raExtraido]);
-        if (buscaAluno.rows.length === 0)
-        {
-            return res.status(404).json({erro: 'Aluno não foi encontrado.'});
-        }
-
-        const Aluno = buscaAluno.rows[0];
-
+        //Até aqui, encontramos um OTP válido, o que significa que podemos validar o login do aluno.
         if (Aluno.votou)
         {
-            return res.status(403).json({erro: 'Seu voto já foi computado nessa eleição.'});
+            return res.status(403).json({mensagem: `Seu voto já foi computado nesta eleição.`});
         }
 
+        const deletar = await db.query(`UPDATE tb_alunos SET otp = NULL, otp_data = NULL WHERE ra = $1`, [ra]);
+
         const TokenUrna = jwt.sign(
-            { id: aluno.id, tipo: 'aluno', serie: Aluno.serie},
+            { id: Aluno.id, tipo: 'aluno', serie: Aluno.serie},
             process.env.JWT_SECRET,
             { expiresIn: '30m'}
         );
 
         res.status(200).json({
-            mensagem: 'Autenticado com sucesso via Google!',
+            mensagem: 'Autenticado com sucesso via OTP!',
             token: TokenUrna,
             aluno: {
                 nome: Aluno.nome,
                 serie: Aluno.serie 
             }
         });
-    }
-    catch (erro)
+    } catch(erro)
     {
-        console.error(erro);
-        res.status(401).json({ erro: 'Falha na autenticação com o Google.'});
+        console.log(erro);
+        return res.status(500).json({erro: 'Não foi possível verificar o OTP.'});
     }
-};
-*/
+}
+
+//Autenticação de Admin ou Auditor
 
 async function authAdmin_or_Auditor(req, res) {
 
@@ -116,5 +152,4 @@ async function authAdmin_or_Auditor(req, res) {
     }
 };
 
-// Assim que implementar o login do aluno, exportá-lo para o router.
-module.exports = {authAdmin_or_Auditor};
+module.exports = {authAdmin_or_Auditor, solicitarOTP, verificarOTP};
